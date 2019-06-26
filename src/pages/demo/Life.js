ae0560be97002096502ad785ae76c8e07568c79d
古晓雄 <xiaoxiong.gu@ruisdata.com>
2019-06-26 17:17:42 +0800

    react 基础

---
+++ src/pages/demo/Life.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default class Lify extends React.Component{
+
+  constructor(props){
+    super(props)
+    this.state={
+      count:0
+    };
+  }
+
+  handleAdd=()=>{
+    this.setState({
+      count:this.state.count + 1
+    })
+  }
+
+  handleClick(){
+    this.setState({
+      count:this.state.count + 1
+    })
+  }
+  render()
+  {
+    return <div>
+      <p>React 生命周期介绍</p>
+      <button onClick={this.handleAdd}>点击一下</button>
+      <button onClick={this.handleClick.build(this)}>点击一下</button>
+      <p>{this.state.count}</p>
+    </div>
+  }}
